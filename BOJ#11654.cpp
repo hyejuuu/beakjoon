43b6ecc1d54e7e07ce0117ece776295ea3555279
이혜주 <hyeju.tech@gmail.com>
2018-05-20 13:54:56 +0900

    BOJ 11654번 아스키 코드

---
+++ BOJ#11654.cpp
@@ -0,0 +1,12 @@
+//https://www.acmicpc.net/problem/11654
+//BOJ 11654번 아스키 코드
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    char N;
+    cin>>N;
+    printf("%d\n",N);
+    return 0;
+}
