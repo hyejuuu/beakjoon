6987a2f40112e71f2076f249ef32fb433c403586
이혜주 <hyeju.tech@gmail.com>
2018-05-25 23:09:55 +0900

    BOJ 14501 퇴사

--- BOJ#14501.cpp
+++ BOJ#14501.cpp
@@ -1,26 +1,24 @@
 //https://www.acmicpc.net/problem/14501
-//BOJ 14501번 퇴사
+//BOJ 14501 퇴사
 
 #include <iostream>
-#include <cstring>
-#include <algorithm>
 using namespace std;
 
-int N,arr[16][2],mx=0;
+int N,table[15][15],mx=0;;
 
-void func(int cur,int sum){
-    if(cur==N){
-        mx=max(mx,sum);
+void func(int day,int sum){
+    if(day>=N){
+        if(mx<sum)mx=sum;
         return;
     }
-    if(cur+1<=N) func(cur+1,sum);
-    if(cur+arr[cur][0]<=N) func(cur+arr[cur][0],sum+arr[cur][1]);
+    func(day+1,sum);
+    if(day+table[day][0]<=N) func(day+table[day][0],sum+table[day][1]);
 }
 
 int main(){
     cin>>N;
-    for(int i=0;i<N;i++) cin>>arr[i][0]>>arr[i][1];
-    for(int i=0;i<N;i++) func(i,0);
+    for(int i=0;i<N;i++)cin>>table[i][0]>>table[i][1];
+    func(0,0);
     printf("%d\n",mx);
     return 0;
 }
