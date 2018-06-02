6572037a27799212e00f9ec932e9f1dc138db69d
이혜주 <hyeju.tech@gmail.com>
2018-06-02 14:05:52 +0900

    BOJ 3046 R2

---
+++ BOJ#3046.cpp
@@ -0,0 +1,12 @@
+//https://www.acmicpc.net/problem/3046
+//BOJ 3046 R2
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int R1,S;
+    cin>>R1>>S;
+    printf("%d\n",2*S-R1);
+    return 0;
+}
